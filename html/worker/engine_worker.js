6764d091451b0e9ed728fe555ba28f8d5dfcdd29
mzki <28139969+mzki@users.noreply.github.com>
2024-12-22 14:13:56 +0000

    html: Add license comment  at header of js file

--- html/worker/engine_worker.js
+++ html/worker/engine_worker.js
@@ -1,3 +1,7 @@
+// Copyright 2024 The erago-wasm Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
 importScripts("wasm_exec.js");
 
 if (!WebAssembly.instantiateStreaming) { // polyfill
